1bef5d272a9f49c0687b202745eb9b6df94766bf
eddyb <eddyboughioul@hotmail.fr>
2023-02-08 18:07:59 +0100

    update infos

--- src/onboarding/2-create-project.ts
+++ src/onboarding/2-create-project.ts
@@ -29,9 +29,9 @@ const component = '[IMX-CREATE-PROJECT]';
    * Edit your values here
    */
   const params: CreateProjectParams = {
-    name: 'ENTER_PROJECT_NAME_HERE-2',
-    company_name: 'ENTER_COMPANY_NAME_HERE',
-    contact_email: 'contactemail@example.com',
+    name: 'Game Test',
+    company_name: 'BE Technology',
+    contact_email: '###@hotmail.fr',
   };
 
   let project;
